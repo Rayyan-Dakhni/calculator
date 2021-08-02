49d0f2df0e6be8ef63e606e6fc4ebbc1dc0304d0
RayyanDakhni <dakhnirayyan@gmail.com>
2021-08-02 14:23:34 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="calculator-screen neumorphism">
+        <div className="screen neumorphism">
+          <p className="answer-screen">10</p>
+        </div>
+        <div className="button-holder neumorphism">
+          <div className="row">
+            <div className="col neumorphism">7</div>
+            <div className="col neumorphism">8</div>
+            <div className="col neumorphism">9</div>
+            <div className="col neumorphism">AC</div>
+          </div>
+          <div className="row">
+            <div className="col neumorphism">7</div>
+            <div className="col neumorphism">8</div>
+            <div className="col neumorphism">9</div>
+            <div className="col neumorphism">*</div>
+          </div>
+          <div className="row">
+            <div className="col neumorphism">4</div>
+            <div className="col neumorphism">5</div>
+            <div className="col neumorphism">6</div>
+            <div
+              className="col neumorphism"
+              style={{ backgroundColor: "black", color: "white" }}
+            >
+              /
+            </div>
+          </div>
+          <div className="row">
+            <div className="col neumorphism">1</div>
+            <div className="col neumorphism">2</div>
+            <div className="col neumorphism">3</div>
+            <div
+              className="col neumorphism"
+              style={{ backgroundColor: "black", color: "white" }}
+            >
+              +
+            </div>
+          </div>
+          <div className="row">
+            <div className="col neumorphism"></div>
+            <div className="col neumorphism">0</div>
+            <div
+              className="col neumorphism"
+              style={{ backgroundColor: "orange" }}
+            >
+              =
+            </div>
+            <div
+              className="col neumorphism"
+              style={{ backgroundColor: "black", color: "white" }}
+            >
+              -
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
